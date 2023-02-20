573c746e42fe0ca15d8fb55358d3d34f285c966a
mikolajmki <mikol4m@gmail.com>
2023-02-21 00:06:30 +0100

    mobile fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,14 @@ import { Footer } from "./components/Footer/Footer";
 
 const App = () => {
   //don't forget to add font link in index.html
+
+  const scrollEventHandler = function()
+  {
+    window.scroll(0, window.pageYOffset)
+  }
+  
+  window.addEventListener("scroll", scrollEventHandler, false);
+
   return (
     <div className={`bg-primary ${css.container}`}>
       <Header/>
